55cded8fae8472a056b2b19adfa9597295e3051d
macDomenico <d.neri@uurla.it>
2017-09-07 17:53:13 +0200

    Initial checkin

---
+++ udrepapp/udrepapp-db/src/main/java/org/uurla/ud/udrepapp/db/App.java
@@ -0,0 +1,13 @@
+package org.uurla.ud.udrepapp.db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
